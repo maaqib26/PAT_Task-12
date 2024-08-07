b8309f1567c7ce632e009c2ae37c16d8dd97afb8
maaqib26 <160389428+maaqib26@users.noreply.github.com>
2024-08-08 00:54:23 +0530

    Update excel_functions.py

--- Utilities/excel_functions.py
+++ Utilities/excel_functions.py
@@ -32,22 +32,6 @@ class Excel_Operations:
         sheet = workbook[self.sheet_number]
         sheet.cell(row=row_number,column=column_number).value = data
         workbook.save(self.filename)
-
-# if __name__=="__main__":
-#     excel_file = "E:\\Workspace\\GUVI\\PAT-Tasks\\PAT-Task-12\\Data\\test_data.xlsx"
-#     sheet_number = "Sheet1"
-#     data = [1,2,3,4,5]
-#
-#     excel_obj = Excel_Operations(excel_file,sheet_number)
-#     row = excel_obj.row_count()
-#     print(excel_obj.row_count())
-#     print(excel_obj.column_count())
-#     print(excel_obj.read_data(1,4))
-#     # for row in range(2, row+1):
-#     #     for i in range(len(data)):
-#     #         excel_obj.write_data(row_number=row,column_number=8,data=data[i])
-#     start_row = 2
-#     for i in range(len(data)):
 #         excel_obj.write_data(row_number=start_row + i, column_number=8, data=data[i])
 
 
