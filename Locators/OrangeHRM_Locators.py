499fcd804de888228fdaedd481933ee82b857d57
maaqib26 <160389428+maaqib26@users.noreply.github.com>
2024-08-08 00:47:16 +0530

    Add files via upload

---
+++ Locators/OrangeHRM_Locators.py
@@ -0,0 +1,12 @@
+class Locators:
+    username = "username"
+    password = "password"
+    submit_button = "//button[@type='submit']"
+    logout_dropdown = 'oxd-userdropdown-tab'
+    logout = 'Logout'
+    url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    dashboard_url = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    excel_file = "E:\\Workspace\\GUVI\\PAT-Tasks\\PAT-Task-12\\Data\\test_data.xlsx"
+    sheet_number = "Sheet1"
+    pass_data = "TEST PASS"
+    fail_data = "TEST FAILED"
